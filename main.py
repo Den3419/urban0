da1a0fd6216528cc66ba8681ee94443993287f47
Den3419 <162421329+Den3419@users.noreply.github.com>
2024-08-16 16:25:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print(5 *  9 ** 0.5 )  #"1st program"
+print(9.99 > 9.98  and 1000 != 1001)  #"2nd program"
+print(2 * 2 + 2 )#"3rd program"
+print((2 + 2 ) * 2)
+print((2 * 2 + 2) == ((2 +  2) * 2))
+print((type('123.456')),(float('123.456') * 10  % 10))#"4th program"
+print(int(float('123.456') * 10 % 10 )) #"4th program"
+
