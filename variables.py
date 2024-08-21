ca17a7a89e3e5a1fa90061102e2b9c19a0d5de90
Den3419 <162421329+Den3419@users.noreply.github.com>
2024-08-21 21:41:37 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+the_number_of_completed_dz = 12
+number_of_hours_spent = 1.5
+сourse_name = "Python"
+time_for_one_task = number_of_hours_spent / the_number_of_completed_dz
+print(сourse_name,'всего_задач',the_number_of_completed_dz,'затрачено_часов',number_of_hours_spent)
+print('время_выполнение',time_for_one_task )
