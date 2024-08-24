3a56b4a9db3fb75f11f2c2111128de31a28e8f2a
Den3419 <162421329+Den3419@users.noreply.github.com>
2024-08-24 18:53:15 +0700

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,13 +1,11 @@
-from msilib.schema import tables
-
-my_dict = {'Denis': 1987, 'Alexa': 1985, 'Ivan': 1980 }
+my_dict = {'Denis': 1987, 'Alexa': 1985, 'Ivan': 1980}
 print(my_dict)
 print(my_dict['Alexa'] )
 my_dict['Ivan'] = 2000
 print(my_dict)
 my_dict.update({'Sergei':2001,'Alexsey':2005})
 print(my_dict)
-del my_dict['Sergei']
+a = my_dict.pop('Sergei')
 print(my_dict)
 my_set = {1,'apple',42,111.54,1,42}
 print(my_set)
