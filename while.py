8b1b2311002f5723d8a886359458d366d59b2900
Den3419 <162421329+Den3419@users.noreply.github.com>
2024-08-27 17:48:48 +0700

    Add files via upload

--- while.py
+++ while.py
@@ -1,4 +1,6 @@
-numbers = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
-for number in numbers[:4]:
-    print(number)
+i = 0
+while i < len(my_list) and my_list[i] > 0:
+    print(my_list[i])
+    i += 1
